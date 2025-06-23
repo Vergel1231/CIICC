ea594c483530d433ac00a26c16d2a70709426f10
Vergel1231 <69107513+Vergel1231@users.noreply.github.com>
2025-06-23 07:54:48 +0000

    Created primitives with different values.

---
+++ Task2.java
@@ -0,0 +1,17 @@
+public class Task2 {
+    public static void main(String[] args) {
+        // Declare primitive variables
+        char H = 'H';
+        byte three = 3;
+        short one = 1;
+        int zero = 0;
+        float twoPointZero = 2.0f;
+        boolean isTrue = true;
+
+        // Build the string
+        String output = "" + H + three + one + one + zero + " w" + zero + "r" + one + "d " + twoPointZero + " " + isTrue;
+
+        // Print the result
+        System.out.println(output);
+    }
+}
